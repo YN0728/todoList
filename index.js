71bab0a6d0e98f3774d46760c53c408bc140ec92
Nataly Yayloyan <nataly.yayloyan@gmail.com>
2020-06-10 01:38:27 +0400

    todo draft1

---
+++ index.js
@@ -0,0 +1,59 @@
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+	var span = document.createElement("SPAN");
+	var txt = document.createTextNode("\u00D7");
+	span.className = "close";
+	span.appendChild(txt);
+	myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+	close[i].onclick = function() {
+		var div = this.parentElement;
+		div.style.display = "none";
+	}
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+	if (ev.target.tagName === 'LI') {
+		ev.target.classList.toggle('checked');
+	}
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+	var li = document.createElement("li");
+	var inputValue = document.getElementById("myInput").value;
+	var statusHeader = document.getElementById("statusHeader").value;
+	console.log(statusHeader);
+	var t = document.createTextNode(inputValue);
+	li.appendChild(t);
+	if (inputValue === '') {
+		alert("You must write something!");
+	} else {
+		if(statusHeader==="stuck") document.getElementById("stuckList").appendChild(li);
+		else if(statusHeader==="todo") document.getElementById("todoList").appendChild(li);
+		else if(statusHeader==="inprogress") document.getElementById("progressList").appendChild(li);
+		else if(statusHeader==="done") document.getElementById("doneList").appendChild(li);
+	}
+	document.getElementById("myInput").value = "";
+
+	var span = document.createElement("SPAN");
+	var txt = document.createTextNode("\u00D7");
+	span.className = "close";
+	span.appendChild(txt);
+	li.appendChild(span);
+
+	for (i = 0; i < close.length; i++) {
+		close[i].onclick = function() {
+			var div = this.parentElement;
+			div.style.display = "none";
+		}
+	}
+}
